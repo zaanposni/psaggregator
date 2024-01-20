b159707f35922d07913bfc489c6a70ef27a92ad7
zaanposni <me@zaanposni.com>
2024-01-20 16:20:57 +0100

    ms psaggregator docker ready

--- src/psaggregator/src/lib/prisma.ts
+++ src/psaggregator/src/lib/prisma.ts
@@ -1,4 +1,6 @@
-import { PrismaClient } from "@prisma/client";
+import { createRequire } from "module"; // https://github.com/prisma/prisma/issues/15614
+const require = createRequire(import.meta.url);
+const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
