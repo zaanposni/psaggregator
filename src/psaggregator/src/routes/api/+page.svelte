40e66048c2b6f7e6fbeb8df66d7e6f0de2d22bf7
zaanposni <me@zaanposni.com>
2024-01-21 00:10:49 +0100

    add api doc, closes #1

--- src/psaggregator/src/routes/api/+page.svelte
+++ src/psaggregator/src/routes/api/+page.svelte
@@ -1,1 +1,90 @@
-krasse api
+<script lang="ts">
+    import { CodeBlock } from "@skeletonlabs/skeleton";
+    import hljs from "highlight.js/lib/core";
+    import bash from "highlight.js/lib/languages/bash";
+    import { storeHighlightJs } from "@skeletonlabs/skeleton";
+    import moment from "moment";
+
+    const curlUploadPlan = `curl -X GET ${location.protocol}//${location.host}/api/uploadplan?date=${moment().format("YYYY-MM-DD")}`;
+    const curlVideos = `curl -X GET ${location.protocol}//${location.host}/api/videos?skip=0`;
+    const curlTwitch = `curl -X GET ${location.protocol}//${location.host}/api/twitch`;
+    const curlThumbnails = `curl -X GET ${location.protocol}//${location.host}/api/thumbnails?skip=0`;
+    const curlReddit = `curl -X GET ${location.protocol}//${location.host}/api/reddit`;
+    const curlInformation = `curl -X GET ${location.protocol}//${location.host}/api/information?skip=0&date=${moment().format("YYYY-MM-DD")}`;
+
+    hljs.registerLanguage("bash", bash);
+    storeHighlightJs.set(hljs);
+</script>
+
+<style lang="postcss">
+    .api > div {
+        @apply mt-4 flex flex-col;
+    }
+</style>
+
+<div class="api flex flex-col p-4 md:p-8">
+    <span class="text-2xl font-bold md:text-4xl">API</span>
+    <div>
+        <span>Die API ist eine klassische JSON HTTP API.</span>
+        <span>Die Base URL ist {location.protocol}//{location.host}/api </span>
+        <span
+            >Auf diese API kann nur lesend zugegriffen werden. Datengrundlage sind Importe von der pietsmiet.de Website sowie von
+            Social-Media Plattformen.</span>
+    </div>
+    <span class="text-1xl mt-4 font-bold md:mt-8 md:text-2xl">GET /uploadplan</span>
+    <div class="xl:w-1/2">
+        <CodeBlock language="bash" code={curlUploadPlan} />
+    </div>
+    <div>
+        <span>Der Uploadplan wird direkt von der pietsmiet.de importiert.</span>
+        <span>Nutze den ?date Parameter um alte Uploadpläne anzusehen.</span>
+    </div>
+    <span class="text-1xl mt-4 font-bold md:mt-8 md:text-2xl">GET /videos</span>
+    <div class="xl:w-1/2">
+        <CodeBlock language="bash" code={curlVideos} />
+    </div>
+    <div>
+        <span>Videos werden ebenfalls direkt von der pietsmiet.de importiert.</span>
+        <span>Es werden maximal 20 Videos importiert - sortiert nach dem Uploaddatum.</span>
+        <span>Der ?skip Parameter kann genutzt werden um weitere Videos zu laden.</span>
+    </div>
+    <span class="text-1xl mt-4 font-bold md:mt-8 md:text-2xl">GET /twitch</span>
+    <div class="xl:w-1/2">
+        <CodeBlock language="bash" code={curlTwitch} />
+    </div>
+    <div>
+        <span>Der aktuelle Twitchstatus wird direkt über die Twitch Helix API importiert.</span>
+        <span>Sollte der PietSmiet Account gerade offline sein, wird "null" zurückgegeben.</span>
+    </div>
+    <span class="text-1xl mt-4 font-bold md:mt-8 md:text-2xl">GET /thumbnails</span>
+    <div class="xl:w-1/2">
+        <CodeBlock language="bash" code={curlThumbnails} />
+    </div>
+    <div>
+        <span>Die Thumbnails bietet dir ähnlich wie /videos eine Übersicht aller Videos. Jedoch in kompakter Form.</span>
+        <span>Dieser Endpunkt wird für die Seite "Thumbnails" genutzt.</span>
+    </div>
+    <span class="text-1xl mt-4 font-bold md:mt-8 md:text-2xl">GET /reddit</span>
+    <div class="xl:w-1/2">
+        <CodeBlock language="bash" code={curlReddit} />
+    </div>
+    <div>
+        <span>Die aktuellen populären Posts auf dem r/pietsmiet.</span>
+    </div>
+    <span class="text-1xl mt-4 font-bold md:mt-8 md:text-2xl">GET /information</span>
+    <div class="xl:w-1/2">
+        <CodeBlock language="bash" code={curlInformation} />
+    </div>
+    <div>
+        <span>Verschiedene Communityposts in Textform</span>
+        <span>Zum Beispiel der Text im Uploadplan, solange dieser nicht der Standardtext ist.</span>
+        <span>Zukünftig sind hier noch Importe aus anderen Social Media geplant.</span>
+    </div>
+    <span class="text-1xl mt-4 font-bold md:mt-8 md:text-2xl">Ausblick</span>
+    <div>
+        <span>Du vermisst etwas?</span>
+        <span>Die API ist noch nicht vollständig. Es werden noch weitere Endpunkte folgen.</span>
+        <span>Wenn du eine Idee hast, melde dich gerne per Mail oder auf GitHub.</span>
+        <span>Dieses Projekt ist Open Source. Du kannst gerne auch selbst einen Pull Request erstellen.</span>
+    </div>
+</div>
