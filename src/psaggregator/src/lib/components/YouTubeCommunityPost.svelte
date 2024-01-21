941cabf6acf0a920cc2fdebd8df4b7c456ae4b29
zaanposni <me@zaanposni.com>
2024-01-21 17:03:19 +0100

    finish information page

---
+++ src/psaggregator/src/lib/components/YouTubeCommunityPost.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import type { Information } from "@prisma/client";
+    import moment from "moment";
+
+    export let post: Information;
+</script>
+
+<a class="card card-hover flex flex-col gap-x-4 p-4" href={post.href} target="_blank">
+    {#if post.date}
+        <div class="mb-2 text-sm">{moment(post.date).fromNow()}</div>
+    {/if}
+    <div>{post.text}</div>
+    {#if post.imageUri}
+        <img class="m-4 rounded-xl md:m-8" src={post.imageUri} alt={"community post attachment"} />
+    {/if}
+</a>
--- src/psaggregator/src/routes/information/+page.svelte
+++ src/psaggregator/src/routes/information/+page.svelte
@@ -1,1 +1,72 @@
-information
+<script lang="ts">
+    import YouTubeCommunityPost from "$lib/components/YouTubeCommunityPost.svelte";
+    import type { PageData } from "./$types";
+    import { LogoYoutube, LogoTwitter, LogoInstagram, FaceDissatisfied } from "carbon-icons-svelte";
+
+    export let data: PageData;
+</script>
+
+<div class="grid grid-cols-1 gap-4 p-4 md:grid-cols-2 md:gap-8 md:p-8 xl:grid-cols-4">
+    <div>
+        <div class="mb-2 ml-2 flex items-center text-2xl">
+            <LogoYoutube size={32} class="mr-2" />
+            YouTube
+        </div>
+        <div class="flex flex-col gap-y-4">
+            {#each data.youtubeCommunityPosts as youtube}
+                <YouTubeCommunityPost post={youtube} />
+            {/each}
+        </div>
+    </div>
+    <div>
+        <div class="mb-2 ml-2 flex items-center text-2xl">
+            <LogoInstagram size={32} class="mr-2" />
+            Instagram
+        </div>
+        <div class="mx-auto flex flex-col items-center text-center">
+            <div>
+                <FaceDissatisfied size={32} />
+            </div>
+            <span>Leider gibt es noch keinen Instagram-Import.</span>
+            <span>Dieses Projekt ist Open Source.</span>
+            <span
+                >Beteilige dich gerne auf
+                <a href="https://github.com/zaanposni/psaggregator" class="underline" target="_blank">GitHub</a>
+            </span>
+        </div>
+    </div>
+    <div>
+        <div class="mb-2 ml-2 flex items-center text-2xl">
+            <img alt="threads" src="/threads-logo.svg" class="mr-2 inline-block h-8 w-8" />
+            Threads
+        </div>
+        <div class="mx-auto flex flex-col items-center text-center">
+            <div>
+                <FaceDissatisfied size={32} />
+            </div>
+            <span>Leider gibt es noch keinen Threads-Import.</span>
+            <span>Dieses Projekt ist Open Source.</span>
+            <span
+                >Beteilige dich gerne auf
+                <a href="https://github.com/zaanposni/psaggregator" class="underline" target="_blank">GitHub</a>
+            </span>
+        </div>
+    </div>
+    <div>
+        <div class="mb-2 ml-2 flex items-center text-2xl">
+            <LogoTwitter size={32} class="mr-2" />
+            Twitter
+        </div>
+        <div class="mx-auto flex flex-col items-center text-center">
+            <div>
+                <FaceDissatisfied size={32} />
+            </div>
+            <span>Leider gibt es noch keinen Twitter-Import.</span>
+            <span>Dieses Projekt ist Open Source.</span>
+            <span
+                >Beteilige dich gerne auf
+                <a href="https://github.com/zaanposni/psaggregator" class="underline" target="_blank">GitHub</a>
+            </span>
+        </div>
+    </div>
+</div>
