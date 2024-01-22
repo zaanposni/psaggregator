ee10e879a7807101834fc6fc3642412942d7ada9
zaanposni <me@zaanposni.com>
2024-01-22 19:38:10 +0100

    add openai information to planned stream

--- src/psaggregator/src/lib/components/UploadPlanEntry.svelte
+++ src/psaggregator/src/lib/components/UploadPlanEntry.svelte
@@ -2,13 +2,14 @@
     import type { ScheduledContentPiece } from "@prisma/client";
     import { Video, VideoPlayer } from "carbon-icons-svelte";
     import moment from "moment";
+    import Sparkle from "./Sparkle.svelte";
 
     export let entry: ScheduledContentPiece;
 </script>
 
 {#if entry.href}
-    <a class="card card-hover flex items-center gap-x-4 p-4" href={entry.href} target="_blank">
-        <div class="shrink-0">
+    <a class="card card-hover flex items-center p-4" href={entry.href} target="_blank">
+        <div class="mr-4 shrink-0">
             {#if entry.type === "TwitchStream"}
                 <Video size={32} />
             {:else}
@@ -17,7 +18,7 @@
         </div>
         {#if entry.startDate}
             {@const date = moment(entry.startDate)}
-            <div class="shrink-0">
+            <div class="mr-4 shrink-0">
                 {#if !moment().isSame(date, "day")}
                     <span class="font-bold">{date.format("DD. MMM,")}</span>
                 {/if}
@@ -25,10 +26,16 @@
             </div>
         {/if}
         <span class="overflow-x-hidden text-ellipsis whitespace-nowrap" title={entry.title}>{entry.title}</span>
+        {#if entry.importedFrom === "OpenAI"}
+            <div class="grow" />
+            <div class="shrink-0" title="Diese Ressource wurde von der OpenAI Vision AI erstellt">
+                <Sparkle />
+            </div>
+        {/if}
     </a>
 {:else}
-    <div class="card flex items-center gap-x-4 p-4" title="Noch nicht veröffentlicht">
-        <div class="shrink-0">
+    <div class="card flex items-center p-4" title="Noch nicht veröffentlicht">
+        <div class="mr-4 shrink-0">
             {#if entry.type === "TwitchStream"}
                 <Video size={32} />
             {:else}
@@ -37,7 +44,7 @@
         </div>
         {#if entry.startDate}
             {@const date = moment(entry.startDate)}
-            <div class="shrink-0">
+            <div class="mr-4 shrink-0">
                 {#if !moment().isSame(date, "day")}
                     <span class="font-bold">{date.format("DD. MMM,")}</span>
                 {/if}
@@ -45,5 +52,11 @@
             </div>
         {/if}
         <span class="overflow-x-hidden text-ellipsis whitespace-nowrap" title={entry.title}>{entry.title}</span>
+        {#if entry.importedFrom === "OpenAI"}
+            <div class="grow" />
+            <div class="shrink-0" title="Diese Ressource wurde von der OpenAI Vision AI erstellt">
+                <Sparkle />
+            </div>
+        {/if}
     </div>
 {/if}
