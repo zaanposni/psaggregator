e28f4beaccff969bc3c48f37a238d6d49fd255e4
zaanposni <me@zaanposni.com>
2024-01-26 19:11:39 +0100

    add complexe view to videos page

--- src/psaggregator/src/lib/components/PSVideo.svelte
+++ src/psaggregator/src/lib/components/PSVideo.svelte
@@ -3,6 +3,10 @@
     import moment from "moment";
 
     export let video: ContentPiece;
+    export let lineClamp: 2 | 3 | 4 | 5 | 6 = 2;
+
+    let classes = "";
+    export { classes as class };
 
     $: humanReadableMinutes = video.duration ? Math.floor(video.duration / 60) : null;
     $: humanReadableSeconds = video.duration ? video.duration % 60 : null;
@@ -18,27 +22,27 @@
         background: linear-gradient(0deg, rgba(0, 0, 0, 0.6) 0, transparent 40%);
         transition: background 0.2s;
     }
+
+    .line-clamp {
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+    }
 </style>
 
-<a
-    class="card relative aspect-square h-full w-64"
-    href={video.href}
-    target="_blank"
-    title={video.title}>
+<a class="card relative aspect-square h-full w-64 {classes}" href={video.href} target="_blank" title={video.title}>
     <div class="block">
         <div class="relative aspect-video">
             <img class="h-full w-full object-cover" src={video.imageUri} alt={video.title} />
             <div class="overlap"></div>
             {#if humanReadableMinutes !== null && humanReadableSeconds !== null}
                 <span class="absolute bottom-0 right-0 m-2 text-xs font-bold text-white"
-                    >{("00" + humanReadableMinutes).slice(-2)}:{("00" + humanReadableSeconds).slice(
-                        -2
-                    )}</span>
+                    >{("00" + humanReadableMinutes).slice(-2)}:{("00" + humanReadableSeconds).slice(-2)}</span>
             {/if}
         </div>
     </div>
     <div>
-        <div class="m-4 line-clamp-2 font-bold">
+        <div class="line-clamp m-4 font-bold" style="-webkit-line-clamp: {lineClamp}">
             {video.title}
         </div>
     </div>
--- src/psaggregator/src/routes/videos/+page.svelte
+++ src/psaggregator/src/routes/videos/+page.svelte
@@ -3,12 +3,15 @@
     import type { PageData } from "./$types";
     import moment from "moment";
     import { browser } from "$app/environment";
-    import { ProgressRadial } from "@skeletonlabs/skeleton";
+    import { ProgressRadial, SlideToggle } from "@skeletonlabs/skeleton";
+    import PsVideo from "$lib/components/PSVideo.svelte";
+    import MediaQuery from "$lib/utils/MediaQuery.svelte";
 
     export let data: PageData;
 
     const batchSize = 50;
 
+    let showAdvancedView = false;
     let previousMonth: string | null = null;
     let skip = 0;
     let loading = false;
@@ -72,26 +75,41 @@
     });
 </script>
 
-<section class="grid grid-cols-2 gap-4 p-4 md:grid-cols-3 md:p-8 xl:grid-cols-5">
-    {#each data.videos as video}
-        {#if video.startDate}
-            {@const newMonth = checkMonth(video.startDate)}
-            {#if newMonth}
-                <div class="col-span-full text-lg font-bold">
-                    {newMonth}
+<MediaQuery query="(min-width: 768px)" let:matches>
+    <div class="p-4 md:p-8">
+        <div class="mb-4 flex w-full flex-col justify-between gap-y-4 md:mb-8 md:flex-row md:items-center">
+            <h1 class="text-3xl font-bold">Alle Videos</h1>
+            <div class="mr-4 flex items-center gap-x-1 md:gap-x-4">
+                <span>Komplexe Ansicht</span>
+                <SlideToggle name="slide" bind:checked={showAdvancedView} />
+            </div>
+        </div>
+        <section class="grid grid-cols-2 gap-4 md:grid-cols-3 xl:grid-cols-5 {!matches && showAdvancedView ? '!grid-cols-1' : ''}">
+            {#each data.videos as video}
+                {#if video.startDate}
+                    {@const newMonth = checkMonth(video.startDate)}
+                    {#if newMonth}
+                        <div class="col-span-full text-lg font-bold">
+                            {newMonth}
+                        </div>
+                    {/if}
+                {/if}
+                {#if showAdvancedView}
+                    <PsVideo {video} class="w-full {matches || showAdvancedView ? '' : '!text-sm'}" lineClamp={matches ? 3 : 2} />
+                {:else}
+                    <a href={video.href} target="_blank" class="overflow-hidden">
+                        <img
+                            class="h-auto max-w-full transform rounded-lg transition-transform duration-500 hover:scale-110"
+                            src={video.imageUri}
+                            alt={video.title} />
+                    </a>
+                {/if}
+            {/each}
+            {#if loading}
+                <div class="col-span-full flex w-full items-center justify-center text-center">
+                    <ProgressRadial />
                 </div>
             {/if}
-        {/if}
-        <a href={video.href} target="_blank" class="overflow-hidden">
-            <img
-                class="h-auto max-w-full transform rounded-lg transition-transform duration-500 hover:scale-110"
-                src={video.imageUri}
-                alt={video.title} />
-        </a>
-    {/each}
-    {#if loading}
-        <div class="col-span-full flex w-full items-center justify-center text-center">
-            <ProgressRadial />
-        </div>
-    {/if}
-</section>
+        </section>
+    </div>
+</MediaQuery>
