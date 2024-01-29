dc4d07b6f3440dcd2d6050ee52d6240070d1015d
zaanposni <me@zaanposni.com>
2024-01-29 16:57:18 +0100

    refactored pietsmietdevideoimporter.py

--- src/dataimport/pietsmietdevideoimporter.py
+++ src/dataimport/pietsmietdevideoimporter.py
@@ -20,7 +20,7 @@ if not os.path.exists("/app/cdn/psde"):
 
 
 async def stuff() -> asyncio.coroutine:
-    console.log("Starting...", style="bold green")
+    console.log("Starting...")
 
     options = webdriver.FirefoxOptions()
     options.add_argument("--headless")
@@ -32,14 +32,14 @@ async def stuff() -> asyncio.coroutine:
         },
     )
 
-    console.log("Loading page...", style="bold green")
+    console.log("Loading page...")
 
     browser.get("https://www.pietsmiet.de/videos")
     browser.implicitly_wait(10)
 
     assert "Videos" in browser.title
 
-    console.log("Loading data...", style="bold green")
+    console.log("Loading data...")
 
     videos = None
     for request in browser.requests:
@@ -57,39 +57,41 @@ async def stuff() -> asyncio.coroutine:
     videos = videos["data"]
 
     console.log(f"Found {len(videos)} entries")
-    console.log("Closing browser...", style="bold green")
+    console.log("Closing browser...")
 
     browser.quit()
 
     with open("videos.json", "w", encoding="utf-8") as f:
         json.dump(videos, f, ensure_ascii=False, indent=4)
 
-    console.log("Dumped data to videos.json", style="bold green")
-    console.log("Parsing data...", style="bold green")
+    console.log("Dumped data to videos.json")
+    console.log("Parsing data...")
 
     data = []
     for video in videos:
         if not video.get("id") or not video.get("title"):
             continue
-        uri = "NULL"
-        imageUri = "NULL"
-        publish_date = "NULL"
+        uri = None
+        imageUri = None
+        publish_date = None
         if video.get("short_url"):
-            uri = f"'{video['short_url']}'"
+            uri = video["short_url"]
         if video.get("thumbnail"):
             try:
-                imageUri = f"{video['thumbnail']['variations'][0]['url']}"
+                imageUri = video["thumbnail"]["variations"][0]["url"]
             except KeyError:
                 pass
             except IndexError:
                 pass
         if video.get("publish_date"):
-            publish_date = f"'{dateutil.parser.parse(video['publish_date']).strftime('%Y-%m-%d %H:%M:%S')}'"
+            publish_date = dateutil.parser.parse(video["publish_date"]).strftime(
+                "%Y-%m-%d %H:%M:%S"
+            )
 
         data.append(
             {
                 "remoteId": video["id"],
-                "title": video["title"].replace("'", "\\'"),
+                "title": video["title"],
                 "duration": video["duration"],
                 "uri": uri,
                 "imageUri": imageUri,
@@ -97,66 +99,72 @@ async def stuff() -> asyncio.coroutine:
             }
         )
 
-    console.log("Connecting to database...", style="bold green")
+    console.log("Connecting to database...")
     db = Database(url=os.getenv("DATABASE_URL"))
     await db.connect()
 
     INSERT_STATEMENT = """
     INSERT INTO ContentPiece (id  , remoteId, title, description, additionalInfo, startDate, imageUri, href, duration, importedAt, importedFrom , type) VALUES
-                             ('{}', '{}'    , '{}' , NULL       , NULL          , {}       , {}      , {}  , {}      , now()     , 'PietSmietDE', 'PSVideo');"""
-    UPDATE_STATEMENT = """
-    UPDATE ContentPiece SET href={}, title='{}', duration={} WHERE id='{}';"""
+                             (:id ,:remoteId,:title, NULL       , NULL          ,:startDate,:imageUri,:href,:duration, now()     , 'PietSmietDE', 'PSVideo');"""
 
-    console.log("Checking for existing entries...", style="bold green")
+    UPDATE_STATEMENT = """
+    UPDATE ContentPiece SET href=:href, title=:title, duration=:duration WHERE id=:id;"""
+
+    console.log("Checking for existing entries...")
     for content in data:
-        query = "SELECT * FROM ContentPiece WHERE remoteId = '{}' AND importedFrom = 'PietSmietDE'".format(
-            content["remoteId"]
+        result = await db.fetch_all(
+            "SELECT * FROM ContentPiece WHERE remoteId = :remoteId AND importedFrom = 'PietSmietDE'",
+            values={"remoteId": content["remoteId"]},
         )
-        result = await db.fetch_all(query)
         if len(result) > 0:
             console.log(
                 f"Found existing entry for {content['remoteId']}. Updating...",
-                style="bold yellow",
+                style="bright_magenta",
             )
 
-            query = UPDATE_STATEMENT.format(
-                content["uri"],
-                content["title"],
-                content["duration"],
-                result[0]["id"],
+            await db.execute(
+                UPDATE_STATEMENT,
+                values={
+                    "href": content["uri"],
+                    "title": content["title"],
+                    "duration": content["duration"],
+                    "id": result[0]["id"],
+                },
             )
-            await db.execute(query)
+
             console.log(f"Updated entry for {content['remoteId']}.")
         else:
             console.log(
                 f"Adding {content['remoteId']} to database...",
-                style="bold yellow",
+                style="bold green",
             )
 
-            if content["imageUri"] != "NULL":
+            if content["imageUri"] != None:
                 try:
                     thumbnail = requests.get(content["imageUri"]).content
                     filename = f"{uuid4()}.jpg"
                     with open(f"/app/cdn/psde/{filename}", "wb") as f:
                         f.write(thumbnail)
-                    content["imageUri"] = f"'/cdn/psde/{filename}'"
+                    content["imageUri"] = f"/cdn/psde/{filename}"
                 except Exception as e:
                     console.log(f"Error downloading thumbnail: {e}", style="bold red")
-                    content["imageUri"] = f"'{content['imageUri']}'"
+                    content["imageUri"] = content["imageUri"]
 
-            query = INSERT_STATEMENT.format(
-                uuid4(),
-                content["remoteId"],
-                content["title"],
-                content["publish_date"],
-                content["imageUri"],
-                content["uri"],
-                content["duration"],
+            await db.execute(
+                INSERT_STATEMENT,
+                values={
+                    "id": uuid4(),
+                    "remoteId": content["remoteId"],
+                    "title": content["title"],
+                    "startDate": content["publish_date"],
+                    "imageUri": content["imageUri"],
+                    "href": content["uri"],
+                    "duration": content["duration"],
+                },
             )
-            await db.execute(query)
 
     await db.disconnect()
-    console.log("Done!", style="bold green")
+    console.log("Done!")
 
 
 asyncio.run(stuff())
