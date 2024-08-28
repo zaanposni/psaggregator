bd3fca6fafe6de1c6d0395ed46b3bcea761dc297
zaanposni <me@zaanposni.com>
2024-08-28 06:09:30 +0200

    also check alt title without emojis

--- src/dataimport/youtube_video_linker.py
+++ src/dataimport/youtube_video_linker.py
@@ -3,6 +3,7 @@ import requests
 import asyncio
 from datetime import timedelta
 import html
+import re
 
 from rich.console import Console
 from databases import Database
@@ -27,6 +28,24 @@ youtube_url_template = "https://youtu.be/{}"
 UPDATE_STATEMENT = "UPDATE ContentPiece SET description = :description, secondaryHref = :secondaryHref WHERE id = :id"
 
 
+def replace_emojis_with_question_mark(text):
+    # Regex pattern to match emojis
+    emoji_pattern = re.compile(
+        "["
+        "\U0001F600-\U0001F64F"  # emoticons
+        "\U0001F300-\U0001F5FF"  # symbols & pictographs
+        "\U0001F680-\U0001F6FF"  # transport & map symbols
+        "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        "\U00002702-\U000027B0"  # dingbats
+        "\U000024C2-\U0001F251"
+        "]+",
+        flags=re.UNICODE,
+    )
+
+    # Replace emojis with "?"
+    return emoji_pattern.sub("?", text)
+
+
 async def youtube():
     console.log("Connecting to database...", style="bold green")
     db = Database(url=os.getenv("DATABASE_URL"))
@@ -45,9 +64,10 @@ async def youtube():
             one_week_before = yt_pbulish_date - timedelta(days=7)
             one_week_after = yt_pbulish_date + timedelta(days=7)
 
-            query = f"SELECT * FROM ContentPiece WHERE title LIKE :title AND startDate BETWEEN :lowerBoundary and :upperBoundary AND importedFrom = 'PietSmietDE' AND type = 'PSVideo'"
+            query = f"SELECT * FROM ContentPiece WHERE (title LIKE :title OR title LIKE :altTitle) AND startDate BETWEEN :lowerBoundary and :upperBoundary AND importedFrom = 'PietSmietDE' AND type = 'PSVideo'"
             values = {
                 "title": title,
+                "altTitle": replace_emojis_with_question_mark(title),
                 "lowerBoundary": one_week_before.strftime("%Y-%m-%d %H:%M:%S"),
                 "upperBoundary": one_week_after.strftime("%Y-%m-%d %H:%M:%S"),
             }
