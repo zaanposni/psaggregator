97928886fea7de3373af6ffea2343caa96abff4d
zaanposni <me@zaanposni.com>
2024-11-10 10:45:39 +0100

    use umami instead of microanalytics

--- src/psaggregator/src/config/config.ts
+++ src/psaggregator/src/config/config.ts
@@ -3,7 +3,7 @@ import { writable } from "svelte/store";
 
 export const MAIL_TO_URL = "mailto:psaggregator@zaanposni.com";
 export const LEGAL_URL = env.PUBLIC_LEGAL_URL;
-export const MICROANALYTICS_ID = env.PUBLIC_MICROANALYTICS_ID;
+export const UMAMI_ID = env.PUBLIC_UMAMI_ID;
 export const KOFI_USERNAME = env.PUBLIC_KOFI_USERNAME;
 
 export const GITHUB_URL = "https://github.com/zaanposni/psaggregator";
