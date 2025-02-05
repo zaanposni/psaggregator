a168f9a46316af582eccf8e95a7dcae45f7649cf
zaanposni <me@zaanposni.com>
2025-02-05 19:50:17 +0100

    added youtube ignore list

---
+++ src/psaggregator/src/config/migrations/20250205184751_add_ignore_yt_videos/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE `IgnoreYouTubeVideos` (
+    `remoteId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`remoteId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
