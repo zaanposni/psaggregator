14cfa1e5db0e6980d2116e848ec9c24fd97d454e
zaanposni <me@zaanposni.com>
2025-02-05 19:46:21 +0100

    find most likely streamplan and show it in twitch tab

--- src/psaggregator/src/routes/+page.server.ts
+++ src/psaggregator/src/routes/+page.server.ts
@@ -5,125 +5,149 @@ export async function load() {
     const upperBound = moment().endOf("day").toDate();
     const lowerBound = moment().startOf("day").toDate();
 
-    const [today, videos, upcomingStreams, redditPosts, youtubeCommunityPosts, instagramPosts, twitterPosts] = await Promise.all([
-        prisma.scheduledContentPiece.findMany({
-            where: {
-                type: {
-                    equals: "PSVideo"
+    const lowerBoundYoutubeStreamplan = moment().startOf("day").subtract(1, "week").toDate();
+
+    const [today, videos, upcomingStreams, redditPosts, youtubeCommunityPosts, instagramPosts, twitterPosts, youtubeStreamplanPost] =
+        await Promise.all([
+            prisma.scheduledContentPiece.findMany({
+                where: {
+                    type: {
+                        equals: "PSVideo"
+                    },
+                    importedFrom: {
+                        equals: "PietSmietDE"
+                    },
+                    startDate: {
+                        lt: upperBound,
+                        gt: lowerBound
+                    }
                 },
-                importedFrom: {
-                    equals: "PietSmietDE"
-                },
-                startDate: {
-                    lt: upperBound,
-                    gt: lowerBound
+                orderBy: {
+                    startDate: "asc"
                 }
-            },
-            orderBy: {
-                startDate: "asc"
-            }
-        }),
-        // ===
-        prisma.contentPiece.findMany({
-            where: {
-                type: {
-                    equals: "PSVideo"
+            }),
+            // ===
+            prisma.contentPiece.findMany({
+                where: {
+                    type: {
+                        equals: "PSVideo"
+                    },
+                    href: {
+                        not: null
+                    },
+                    startDate: {
+                        not: null
+                    }
                 },
-                href: {
-                    not: null
+                orderBy: {
+                    startDate: "desc"
                 },
-                startDate: {
-                    not: null
+                take: 20
+            }),
+            // ===
+            prisma.scheduledContentPiece.findMany({
+                where: {
+                    type: {
+                        equals: "TwitchStream"
+                    },
+                    startDate: {
+                        gt: lowerBound
+                    }
+                },
+                orderBy: {
+                    startDate: "asc"
+                },
+                take: 8
+            }),
+            // ===
+            prisma.redditPost.findMany({
+                orderBy: {
+                    sticky: "desc"
+                },
+                take: 10
+            }),
+            // ===
+            prisma.information.findMany({
+                where: {
+                    importedFrom: {
+                        equals: "YouTube"
+                    },
+                    href: {
+                        not: null
+                    },
+                    date: {
+                        not: null
+                    }
+                },
+                orderBy: {
+                    date: "desc"
+                },
+                take: 5
+            }),
+            // ===
+            prisma.information.findMany({
+                where: {
+                    importedFrom: {
+                        equals: "Instagram"
+                    },
+                    href: {
+                        not: null
+                    },
+                    date: {
+                        not: null
+                    }
+                },
+                include: {
+                    InformationResource: true
+                },
+                orderBy: {
+                    date: "desc"
+                },
+                take: 3
+            }),
+            // ===
+            prisma.information.findMany({
+                where: {
+                    importedFrom: {
+                        equals: "Twitter"
+                    },
+                    href: {
+                        not: null
+                    },
+                    date: {
+                        not: null
+                    }
+                },
+                include: {
+                    InformationResource: true
+                },
+                orderBy: {
+                    date: "desc"
+                },
+                take: 7
+            }),
+            // ===
+            prisma.information.findFirst({
+                where: {
+                    importedFrom: {
+                        equals: "YouTube"
+                    },
+                    href: {
+                        not: null
+                    },
+                    text: {
+                        contains: "stream"
+                    },
+                    date: {
+                        not: null,
+                        gt: lowerBoundYoutubeStreamplan
+                    }
+                },
+                orderBy: {
+                    date: "desc"
                 }
-            },
-            orderBy: {
-                startDate: "desc"
-            },
-            take: 20
-        }),
-        // ===
-        prisma.scheduledContentPiece.findMany({
-            where: {
-                type: {
-                    equals: "TwitchStream"
-                },
-                startDate: {
-                    gt: lowerBound
-                }
-            },
-            orderBy: {
-                startDate: "asc"
-            },
-            take: 8
-        }),
-        // ===
-        prisma.redditPost.findMany({
-            orderBy: {
-                sticky: "desc"
-            },
-            take: 10
-        }),
-        // ===
-        prisma.information.findMany({
-            where: {
-                importedFrom: {
-                    equals: "YouTube"
-                },
-                href: {
-                    not: null
-                },
-                date: {
-                    not: null
-                }
-            },
-            orderBy: {
-                date: "desc"
-            },
-            take: 5
-        }),
-        // ===
-        prisma.information.findMany({
-            where: {
-                importedFrom: {
-                    equals: "Instagram"
-                },
-                href: {
-                    not: null
-                },
-                date: {
-                    not: null
-                }
-            },
-            include: {
-                InformationResource: true
-            },
-            orderBy: {
-                date: "desc"
-            },
-            take: 3
-        }),
-        // ===
-        prisma.information.findMany({
-            where: {
-                importedFrom: {
-                    equals: "Twitter"
-                },
-                href: {
-                    not: null
-                },
-                date: {
-                    not: null
-                }
-            },
-            include: {
-                InformationResource: true
-            },
-            orderBy: {
-                date: "desc"
-            },
-            take: 7
-        })
-    ]);
+            })
+        ]);
 
     return {
         today,
@@ -132,6 +156,7 @@ export async function load() {
         redditPosts,
         youtubeCommunityPosts,
         instagramPosts,
-        twitterPosts
+        twitterPosts,
+        youtubeStreamplanPost
     };
 }
